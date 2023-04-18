7647eb0b52436ad1dafd49b4686d124a90fa9c6f
S8Vega <bsebastianvg18@gmail.com>
2023-04-18 07:40:08 -0500

    add implementation of methods

--- server/src/main/java/com/thechaoscompany/server/grpcserviceimpl/GrpcSchoolServiceImpl.java
+++ server/src/main/java/com/thechaoscompany/server/grpcserviceimpl/GrpcSchoolServiceImpl.java
@@ -2,26 +2,58 @@ package com.thechaoscompany.server.grpcserviceimpl;
 
 import com.thechaoscompany.server.Empty;
 import com.thechaoscompany.server.Id;
-import com.thechaoscompany.server.SchoolObject;
+import com.thechaoscompany.server.School;
 import com.thechaoscompany.server.SchoolServiceGrpc;
 import com.thechaoscompany.server.SchoolsList;
+import com.thechaoscompany.server.entities.SchoolEntity;
 import com.thechaoscompany.server.service.SchoolService;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 @GrpcService
 public class GrpcSchoolServiceImpl extends SchoolServiceGrpc.SchoolServiceImplBase {
     @Autowired
-    private SchoolService schoolService;
+    private SchoolService schoolervice;
 
     @Override
-    public void getOneByID(Id request, StreamObserver<SchoolObject> responseObserver) {
-        super.getOneByID(request, responseObserver);
+    public void getOneByID(Id request, StreamObserver<School> responseObserver) {
+        CompletableFuture<SchoolEntity> foundSchool = schoolervice.getOneById(request.getId());
+        foundSchool.join();
+        School objectSchoolToReturn = School.newBuilder()
+                .setId(foundSchool.join().getId())
+                .setLocation(foundSchool.join().getLocation())
+                .setName(foundSchool.join().getName())
+                .setStatus(foundSchool.join().getStatus())
+                .build();
+        responseObserver.onNext(objectSchoolToReturn);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void getAll(Empty request, StreamObserver<SchoolsList> responseObserver) {
-        super.getAll(request, responseObserver);
+        CompletableFuture<List<SchoolEntity>> completeList = schoolervice.getAll();
+        List<School> schoolsGrpcList = new ArrayList<>();
+        for (int i = 0; i < completeList.join().size(); i++) {
+            School schoolToAdd = School.newBuilder()
+                    .setId(completeList.join().get(i).getId())
+                    .setLocation(completeList.join().get(i).getLocation())
+                    .setName(completeList.join().get(i).getName())
+                    .setStatus(completeList.join().get(i).getStatus())
+                    .build();
+
+            schoolsGrpcList.add(schoolToAdd);
+        }
+        SchoolsList.Builder listToReturn = SchoolsList.newBuilder();
+
+        for (School eachSchool : schoolsGrpcList) {
+            listToReturn.addSchool(eachSchool);
+        }
+        responseObserver.onNext(listToReturn.build());
+        responseObserver.onCompleted();
     }
 }
