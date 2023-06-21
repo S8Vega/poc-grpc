63ec062c0ac10fc4e18d2acd3d5b016049d60f37
s8vega <bsebastianvg18@gmail.com>
2023-06-20 22:11:04 -0500

    remove old projects

--- server/src/main/java/com/thechaoscompany/server/repository/SchoolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thechaoscompany.server.repository;
-
-import com.thechaoscompany.server.entities.SchoolEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<SchoolEntity, Long> {
-}
