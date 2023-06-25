a891c2b7a7656d41ca039354010276b5253c06ec
s8vega <bsebastianvg18@gmail.com>
2023-06-25 15:40:36 -0500

    add dishes on restaurant

--- restaurant-service/src/main/java/com/thechaoscompany/grpc/entity/RestaurantEntity.java
+++ restaurant-service/src/main/java/com/thechaoscompany/grpc/entity/RestaurantEntity.java
@@ -1,13 +1,18 @@
 package com.thechaoscompany.grpc.entity;
 
+import com.thechaoscompany.grpc.Dish;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,4 +24,14 @@ public class RestaurantEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Transient
+    private List<String> dishes;
+
+    public void addDishes(List<Dish.DishGrpc> dishesGrpc) {
+        List<String> dishes = new ArrayList<>();
+        for (Dish.DishGrpc dishGrpc : dishesGrpc) {
+            dishes.add(dishGrpc.getName());
+        }
+        this.dishes = dishes;
+    }
 }
