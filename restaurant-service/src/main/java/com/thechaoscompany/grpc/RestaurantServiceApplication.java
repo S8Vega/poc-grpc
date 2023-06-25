737d1514b1f7274bf75948fb88702419e97df00d
s8vega <bsebastianvg18@gmail.com>
2023-06-25 15:04:32 -0500

    configure database and change package name

--- restaurant-service/src/main/java/com/thechaoscompany/grpc/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/thechaoscompany/grpc/RestaurantServiceApplication.java
@@ -1,4 +1,4 @@
-package com.thechaoscompany.restaurant;
+package com.thechaoscompany.grpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestaurantServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestaurantServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
 
 }
--- restaurant-service/src/test/java/com/thechaoscompany/grpc/RestaurantServiceApplicationTests.java
+++ restaurant-service/src/test/java/com/thechaoscompany/grpc/RestaurantServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.thechaoscompany.restaurant;
+package com.thechaoscompany.grpc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestaurantServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
